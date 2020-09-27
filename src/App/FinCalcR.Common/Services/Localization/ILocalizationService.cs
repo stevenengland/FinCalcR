2c3247e206d68afbd76efe1c386d83fdb724929d
StEn <StEn@steven-england.info>
2020-09-27 10:15:10 +0200

    chore(localization service and error event): ramp up

---
+++ src/App/FinCalcR.Common/Services/Localization/ILocalizationService.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+
+namespace StEn.FinCalcR.Common.Services.Localization
+{
+	public interface ILocalizationService
+	{
+		void ChangeCurrentCulture(CultureInfo cultureInfo);
+
+		T GetLocalizedValue<T>(string key);
+	}
+}
