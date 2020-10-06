cec2623fa48830bb56e4f95faea29ebc252357f8
StEn <StEn@steven-england.info>
2020-10-06 15:52:26 +0200

    feat(Effective interest rate): Add conversion
    Conversion from nominal interest rate to effective interest rate.

---
+++ src/App/FinCalcR.Calculations/InterestCalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StEn.FinCalcR.Calculations
+{
+	public static class InterestCalculator
+	{
+		public static double GetEffectiveInterestRate(double ratesPerAnnum, double yearlyNominalInterest)
+		{
+			var decimalNominalInterest = yearlyNominalInterest / 100;
+			return (Math.Pow(1 + (decimalNominalInterest / ratesPerAnnum), ratesPerAnnum) - 1) * 100;
+		}
+	}
+}
---
+++ src/Tests/FinCalcR.Calculations.Tests/InterestCalculatorTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StEn.FinCalcR.Calculations;
+using Xunit;
+
+namespace FinCalcR.Calculations.Tests
+{
+	public class InterestCalculatorTests
+	{
+		[Theory]
+		[InlineData(12, 4, 4.074)]
+		public void EffectiveInterestRateIsCalculatedCorrectly(double ratesPerAnnum, double yearlyNominalInterest, double expectedEffectiveRate)
+		{
+			var effectiveRate = InterestCalculator.GetEffectiveInterestRate(ratesPerAnnum, yearlyNominalInterest);
+			Assert.Equal(Math.Round(effectiveRate, 3), expectedEffectiveRate);
+		}
+	}
+}
