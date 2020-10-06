9e57969537f88049f66b894ecaf016aa8e3bae4f
StEn <StEn@steven-england.info>
2020-10-06 18:30:22 +0200

    feat(Interst): Add functon to get nominal interest
    Added a new function to calculate the yearly nominal interest rate with the help
    of the effective interest rate.

--- src/App/FinCalcR.Calculations/InterestCalculator.cs
+++ src/App/FinCalcR.Calculations/InterestCalculator.cs
@@ -11,5 +11,11 @@ namespace StEn.FinCalcR.Calculations
 			var decimalNominalInterest = yearlyNominalInterest / 100;
 			return (Math.Pow(1 + (decimalNominalInterest / ratesPerAnnum), ratesPerAnnum) - 1) * 100;
 		}
+
+		public static double GetYearlyNominalInterestRate(double ratesPerAnnum, double effectiveInterestRate)
+		{
+			var decimalEffectiveInterestRate = effectiveInterestRate / 100;
+			return ((Math.Pow(decimalEffectiveInterestRate + 1, 1 / ratesPerAnnum) - 1) * ratesPerAnnum) * 100;
+		}
 	}
 }
--- src/Tests/FinCalcR.Calculations.Tests/InterestCalculatorTests.cs
+++ src/Tests/FinCalcR.Calculations.Tests/InterestCalculatorTests.cs
@@ -10,10 +10,18 @@ namespace FinCalcR.Calculations.Tests
 	{
 		[Theory]
 		[InlineData(12, 4, 4.074)]
-		public void EffectiveInterestRateIsCalculatedCorrectly(double ratesPerAnnum, double yearlyNominalInterest, double expectedEffectiveRate)
+		public void EffectiveInterestRateIsCalculatedCorrectly(double ratesPerAnnum, double yearlyNominalInterest, double expectedEffectiveInterestRate)
 		{
 			var effectiveRate = InterestCalculator.GetEffectiveInterestRate(ratesPerAnnum, yearlyNominalInterest);
-			Assert.Equal(Math.Round(effectiveRate, 3), expectedEffectiveRate);
+			Assert.Equal(Math.Round(effectiveRate, 3), expectedEffectiveInterestRate);
+		}
+
+		[Theory]
+		[InlineData(12, 4, 3.928)]
+		public void YearlyNominalInterestRateIsCalculatedCorrectly(double ratesPerAnnum, double effectiveInterestRate, double expectedNominalInterestRate)
+		{
+			var effectiveRate = InterestCalculator.GetYearlyNominalInterestRate(ratesPerAnnum, effectiveInterestRate);
+			Assert.Equal(Math.Round(effectiveRate, 3), expectedNominalInterestRate);
 		}
 	}
 }
