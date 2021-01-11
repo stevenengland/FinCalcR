0c46223bcea341fd02f8cfa863a9695f3a01aa0c
StEn <StEn@steven-england.info>
2021-01-11 12:15:20 +0100

    feat: Add number to string converter

---
+++ src/App/FinCalcR.WinUi/Converter/NumberToFormattedStringConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace StEn.FinCalcR.WinUi.Converter
+{
+    public class NumberToFormattedStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return "-";
+            }
+
+            if (!double.TryParse(value.ToString(), out var number))
+            {
+                return "-";
+            }
+
+            if (parameter == null)
+            {
+                parameter = 0;
+            }
+
+            var nfi = CultureInfo.CurrentUICulture.NumberFormat;
+            nfi.NumberDecimalDigits = System.Convert.ToInt32(parameter);
+
+            return number.ToString("N", nfi);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotSupportedException();
+    }
+}
---
+++ src/Tests/FinCalcR.WinUi.Tests/ConverterTests/NumberToFormattedStringConverterShould.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Threading;
+using FluentAssertions;
+using StEn.FinCalcR.WinUi.Converter;
+using Xunit;
+
+namespace FinCalcR.WinUi.Tests.ConverterTests
+{
+    public class NumberToFormattedStringConverterShould
+    {
+        public NumberToFormattedStringConverterShould()
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+        }
+
+        [Theory]
+        [InlineData(20.333, 2, "20.33")]
+        [InlineData(20.335, 2, "20.34")]
+        [InlineData(20.333, 0, "20")]
+        [InlineData(20000.333, 2, "20,000.33")]
+        public void ReturnCorrectText_WhenConvertingFromNumber(double input, double precision, string output)
+        {
+            var converter = new NumberToFormattedStringConverter();
+            var result = (string)converter.Convert(input, null, precision, null);
+            result.Should().Be(output);
+        }
+    }
+}
